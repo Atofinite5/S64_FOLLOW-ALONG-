301700e2d9bb862d696c2eea7472a6f9c68cf814
atofinite <bhargav.jagdish@kalvium.community>
2025-02-25 15:35:56 +0530

    milestone21

--- Frontend/src/Pages/createAddress.jsx
+++ Frontend/src/Pages/createAddress.jsx
@@ -19,7 +19,7 @@ const CreateAddress = () => {
             address2,
             zipCode,
             addressType,
-            email: "bhargav1@gmail.com"
+            email: "bhargavkalambhe3@gmail.com"
         };
         try {
             const response = await axios.post(
